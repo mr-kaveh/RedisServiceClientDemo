bea04e3c678c1f68bfa847bd62f114a7ffea2e35
mr-kaveh <mr.hdavoodi@gmail.com>
2022-10-31 18:38:09 +0100

    Update redisclient.js

--- redisclient.js
+++ redisclient.js
@@ -1,20 +1,68 @@
-var redis = require("redis"),
-client = redis.createClient();
+const redis = require('redis');
+const client = redis.createClient();
 
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
-
-client.on("error", function (err) {
-console.log("Error " + err);
+client.on('connect', function() {
+  console.log('Connected!'); // Connected!
 });
 
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-console.log(replies.length + " replies:");
-replies.forEach(function (reply, i) {
-console.log(" " + i + ": " + reply);
+// Strings
+
+client.set('framework', 'ReactJS', function(err, reply) {
+  console.log(reply); // OK
 });
-client.quit();
+
+client.get('framework', function(err, reply) {
+  console.log(reply); // ReactJS
+});
+
+// Hashes
+
+client.hmset('frameworks_hash', 'javascript', 'ReactJS', 'css', 'TailwindCSS', 'node', 'Express');
+
+client.hgetall('frameworks_hash', function(err, object) {
+  console.log(object); // { javascript: 'ReactJS', css: 'TailwindCSS', node: 'Express' }
+});
+
+// Lists
+
+client.rpush(['frameworks_list', 'ReactJS', 'Angular'], function(err, reply) {
+  console.log(reply); // 2
+});
+
+client.lrange('frameworks_list', 0, -1, function(err, reply) {
+  console.log(reply); // [ 'ReactJS', 'Angular' ]
+});
+
+// Sets
+
+client.sadd(['frameworks_set', 'ReactJS', 'Angular', 'Svelte', 'VueJS', 'VueJS'], function(err, reply) {
+  console.log(reply); // 4
+});
+
+client.smembers('frameworks_set', function(err, reply) {
+  console.log(reply); // [ 'Angular', 'ReactJS', 'VueJS', 'Svelte' ]
+});
+
+// Check the existence of a key
+
+client.exists('framework', function(err, reply) {
+  if (reply === 1) {
+    console.log('Exists!');
+  } else {
+    console.log('Doesn\'t exist!');
+  }
+});
+
+// Delete a key
+
+client.del('frameworks_list', function(err, reply) {
+  console.log(reply); // 1
+});
+
+// Increment a key
+
+client.set('working_days', 5, function() {
+  client.incr('working_days', function(err, reply) {
+    console.log(reply); // 6
+  });
 });
