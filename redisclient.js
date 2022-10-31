0c6db6547e96c6dc22a18c4a7be14822a5ee2862
mr-kaveh <mr.hdavoodi@gmail.com>
2022-10-31 18:45:42 +0100

    Update redisclient.js

--- redisclient.js
+++ redisclient.js
@@ -1,68 +1,29 @@
-const redis = require('redis');
-const client = redis.createClient();
+const { Client } = require('pg');
 
-client.on('connect', function() {
-  console.log('Connected!'); // Connected!
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
 });
 
-// Strings
+pgclient.connect();
 
-client.set('framework', 'ReactJS', function(err, reply) {
-  console.log(reply); // OK
+const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
+const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
+const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
+
+pgclient.query(table, (err, res) => {
+    if (err) throw err
 });
 
-client.get('framework', function(err, reply) {
-  console.log(reply); // ReactJS
+pgclient.query(text, values, (err, res) => {
+    if (err) throw err
 });
 
-// Hashes
-
-client.set('frameworks_hash', 'javascript', 'ReactJS', 'css', 'TailwindCSS', 'node', 'Express');
-
-client.hgetall('frameworks_hash', function(err, object) {
-  console.log(object); // { javascript: 'ReactJS', css: 'TailwindCSS', node: 'Express' }
-});
-
-// Lists
-
-client.rpush(['frameworks_list', 'ReactJS', 'Angular'], function(err, reply) {
-  console.log(reply); // 2
-});
-
-client.lrange('frameworks_list', 0, -1, function(err, reply) {
-  console.log(reply); // [ 'ReactJS', 'Angular' ]
-});
-
-// Sets
-
-client.sadd(['frameworks_set', 'ReactJS', 'Angular', 'Svelte', 'VueJS', 'VueJS'], function(err, reply) {
-  console.log(reply); // 4
-});
-
-client.smembers('frameworks_set', function(err, reply) {
-  console.log(reply); // [ 'Angular', 'ReactJS', 'VueJS', 'Svelte' ]
-});
-
-// Check the existence of a key
-
-client.exists('framework', function(err, reply) {
-  if (reply === 1) {
-    console.log('Exists!');
-  } else {
-    console.log('Doesn\'t exist!');
-  }
-});
-
-// Delete a key
-
-client.del('frameworks_list', function(err, reply) {
-  console.log(reply); // 1
-});
-
-// Increment a key
-
-client.set('working_days', 5, function() {
-  client.incr('working_days', function(err, reply) {
-    console.log(reply); // 6
-  });
+pgclient.query('SELECT * FROM student', (err, res) => {
+    if (err) throw err
+    console.log(err, res.rows) // Print the data in student table
+    pgclient.end()
 });
