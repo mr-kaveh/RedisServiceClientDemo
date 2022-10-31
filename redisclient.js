fc80b3201ed7aaddba3138b2f28c691f0e89c930
mr-kaveh <mr.hdavoodi@gmail.com>
2022-10-31 18:09:52 +0100

    Update redisclient.js

--- redisclient.js
+++ redisclient.js
@@ -1,31 +1,20 @@
-const redis = require("redis");
+var redis = require("redis"),
+client = redis.createClient();
 
-// Creates a new Redis client
-// If REDIS_HOST is not set, the default host is localhost
-// If REDIS_PORT is not set, the default port is 6379
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT  
+// if you'd like to select database 3, instead of 0 (default), call
+// client.select(3, function() { /* ... */ });
+
+client.on("error", function (err) {
+console.log("Error " + err);
 });
 
-redisClient.on("error", function(err) {
-    console.log("Error " + err);
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+console.log(replies.length + " replies:");
+replies.forEach(function (reply, i) {
+console.log(" " + i + ": " + reply);
 });
-
-// Sets the key "octocat" to a value of "Mona the octocat"
-redisClient.set("octocat", "Mona the Octocat", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Octopus"
-redisClient.hset("species", "octocat", "Cat and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Dinosaur and Octopus"
-redisClient.hset("species", "dinotocat", "Dinosaur and Octopus", redis.print);
-// Sets a key to "octocat", field to "species", and "value" to "Cat and Robot"
-redisClient.hset(["species", "robotocat", "Cat and Robot"], redis.print);
-// Gets all fields in "species" key
-
-redisClient.hkeys("species", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    redisClient.quit();
+client.quit();
 });
